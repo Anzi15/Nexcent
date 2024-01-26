e7ccfe5a57792f36799524044366b92c02e101ce
Lemme.code <djam4343@gmail.com>
2024-01-26 21:59:50 +0500

    initialization

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html"
+  ],
+  theme: {
+    colors:{
+      blue: 'red'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
