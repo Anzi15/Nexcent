8e0160261eab5264c939dadf49924df61a6c0b21
Lemme.code <djam4343@gmail.com>
2024-02-02 15:00:24 +0500

    add counting animation

--- src/js/observer.js
+++ src/js/observer.js
@@ -2,6 +2,7 @@
 
 //selecting elems from dom
 const allObserverableElems = document.querySelectorAll('[data-observerable_elem]')
+const textCounterElems = document.querySelectorAll("[data-textcounter]")
 
 //creating observer, options new IntersectionObserver and asiging each elem for observations
 const createObserver = function(elemArr){
@@ -22,17 +23,34 @@ const createObserver = function(elemArr){
 }
 
 //manipulating thins accoding to intersection
-function handleIntersect(entries, observer) {
+function handleIntersect(entries) {
     entries.forEach((entry) => {
-        if (entry.isIntersecting) { //when elem comes in viewport
-            entry.target.classList.remove("opacity-0");
-            entry.target.classList.add("opacity-100");
-        } else {
-            //when elem goes out of viewport
-            entry.target.classList.add("opacity-0");
-            entry.target.classList.remove("opacity-100");
+        if(entry.target.dataset.textcounter){
+            if(entry.isIntersecting){
+                console.log('anomation startedx')
+                textcounter(entry.target)
+            }
         }
+            if (entry.isIntersecting) { //when elem comes in viewport
+                entry.target.classList.remove("opacity-0");
+                entry.target.classList.add("opacity-100");
+            } else {
+                //when elem goes out of viewport
+                entry.target.classList.add("opacity-0");
+                entry.target.classList.remove("opacity-100");
+            }
     })
 }
 
+// text counting animation
+function textcounter(elem){
+    const endNum = parseInt(elem.dataset.textcounter);
+    let currentNum = 1;
+    
+    const countAdd = setInterval(() => {
+        currentNum++
+        currentNum > endNum ? clearInterval(countAdd) :  elem.innerText = currentNum;
+    }, 4);
+}
+createObserver(textCounterElems)
 createObserver(allObserverableElems)
