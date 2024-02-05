69f9d310ee266b87e57e3c0bfbebc8155a372777
Lemme.code <djam4343@gmail.com>
2024-02-05 12:55:00 +0500

    footer year updater

--- src/js/index.js
+++ src/js/index.js
@@ -3,15 +3,20 @@
 //selecting elements
 const nav_toggle_btn = document.getElementById('nav-toggle-btn');
 const mob_nav = document.getElementById('mob-nav');
-
+const footerYear = document.getElementById('footer_current_year');
 //functions
 const elemClassToggler = function(elem, classToToggle){
     elem.classList.toggle(classToToggle)
 }
-
+const getCurrentYear = function(){
+    const date = new Date
+    return date.getFullYear()
+}
 //eventListners
 nav_toggle_btn.addEventListener("click",()=>{
     elemClassToggler(mob_nav, "hidden");
 })
-
+window.addEventListener("DOMContentLoaded",()=>{
+    footerYear.innerHTML= getCurrentYear()
+})
 //consoles for debugging
